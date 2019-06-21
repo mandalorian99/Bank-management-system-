549afc16144b8eec9751493f8634a8bb01cadaea
mahendra choudhary <mail.mahendrachoudhary@yahoo.com>
2019-06-21 17:12:28 +0530

    basic blueprint of balance class laid

---
+++ balance.rb
@@ -0,0 +1,19 @@
+# class : Balance
+
+class Balance
+  attr_accessor :id , :customer_id :balance 
+
+  def check_balance
+    # show balance 
+  end
+
+  private
+  def credit
+    # add balance 
+  end
+
+  def debit
+    # reduce blance
+  end
+
+end
