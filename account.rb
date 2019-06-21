0d0cca2e3e57f4bce52f78874b9ac534ba6866e8
mahendra choudhary <mail.mahendrachoudhary@yahoo.com>
2019-06-21 17:11:58 +0530

    Class blueprint laid down

---
+++ account.rb
@@ -0,0 +1,32 @@
+# class : account 
+
+class Account 
+  attr_accessor :accn_id , :accn_number , :accn_type , :accn_balance 
+
+  # public function 
+  def view_account
+    #code here 
+  end
+  
+  def edit_account
+    #code here
+  end
+
+  def find_by_id
+    #code here
+  end
+
+  def self.all
+    # get all accounts
+  end
+   
+  private 
+  def delete_account
+    #code here 
+  end
+
+  def self.add_account
+    #code here 
+  end
+
+end
