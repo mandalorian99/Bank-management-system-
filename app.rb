43add30c9f006acd8b6574aa0c167bbacb805335
mahendra choudhary <mail.mahendrachoudhary@yahoo.com>
2019-06-25 12:54:31 +0530

    staus ok , code working good

--- app.rb
+++ app.rb
@@ -7,30 +7,36 @@ require './account.rb'
 class App
 
   def self.launch
-    #creating a customer 
-    
+=begin
+    #creating a customer    
     c1 = {'customer_id':101 , 'name':'john doe' , 'email':'doe@john.com' , 'phone':'893784'}
     customer = Customer.create(c1)
-    p '----reading customer data ----'
-    p customer.read
 
+    p '----reading customer data ----'
+    p customer.read    
+    
     c2 = {'customer_id':102 , 'name':'abhram' , 'email':'god@lord.com' , 'phone':'999'}
     customer2 = Customer.create(c2)
 
-    #Customer.all
-=begin
+    Customer.all
+    p customer.delete
+    p Customer.count
+
     p "=============="
     e1 = {'emp_id':1002 , 'name':'eva' ,'email':'eva@gmail.com','phone':'94783'}
     emp1 = Employe.create(e1) 
-
+    p emp1.read
     e2 = {'emp_id':1003 , 'name':'angel' ,'email':'angel@hotmail.com','phone':'0000'}
     emp2 = Employe.create(e2)
     
     Employe.all
+    p Employe.count
+    emp1.delete 
+    p emp1.read 
+    p Employe.count 
+
 =end
 
-
-=begin
    a1 = {
      'id':3001 ,
      'account_number':'60033678' ,
@@ -58,13 +64,13 @@ class App
    puts accn2.check_balance(60000999)
 
    p '---------customer balance enquirey------'
-   #puts Customer.new.balance?(60033678)
-   # deleting a account 
-   #Account.delete(accn2)
-   #p Account.count
-   #p Account.delete(accn2)
-   #p Account.all
-=end
+   puts Customer.new.balance?(60033678)
+    
+   Account.delete(accn2)
+   p Account.count
+   p Account.delete(accn2)
+   p Account.all
+
 
 
   end
