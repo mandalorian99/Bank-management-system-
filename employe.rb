1a788251a754713aa53a156cfd2e89baffc964ce
mahendra choudhary <mail.mahendrachoudhary@yahoo.com>
2019-06-25 12:54:13 +0530

    method added to access obj_refs

--- employe.rb
+++ employe.rb
@@ -2,7 +2,25 @@
 # Contain details of employees 
 
 require './user.rb'
+require './crud.rb'
 
 class Employe 
-  attr_accessor :emp_id , :name , :email , :phone  
+  ATTRS =[:emp_id, :name, :email, :phone]
+  attr_accessor(*ATTRS)
+
+  include Crud
+  @@obj_refs = Array.new 
+
+  def initialize
+    @@obj_refs << self 
+  end
+
+  def get_obj_refs
+     @@obj_refs 
+  end 
+
+  def self.get_class_refs
+    @@obj_refs
+  end
+
 end 
