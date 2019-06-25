106a257ea8f3932b2faa95d4e356ddc1dd870553
mahendra choudhary <mail.mahendrachoudhary@yahoo.com>
2019-06-25 18:24:42 +0530

    credit function logic written

--- account.rb
+++ account.rb
@@ -1,89 +1,54 @@
 # class : account 
-require './balance.rb'
+require './crud.rb'
 
-class Account < Balance 
+class Account 
   ATTRS = [:id, :account_number, :account_type, :account_balance]
   attr_accessor(*ATTRS)   
 
   include Crud
-  @@obj_refs = Array.new 
+  @@acn_refs = Array.new 
 
   def initialize
-    @@obj_refs << self 
+    @@acn_refs << self 
   end
 
   def get_obj_refs
-     @@obj_refs 
+     @@acn_refs 
   end 
 
   def self.get_class_refs
-    @@obj_refs
+    @@acn_refs
   end
 
-=begin
-  # public function 
-  def view_account
-    result = '[ '
-    ATTRS.each do |key|
-      result += self.instance_variable_get("@#{key}").to_s 
-      result += ' , '
-    end
-    result += ']'
+  protected
 
-  end
-  
-  def edit_account
-    #code here
-  end
+  def self.balance?(account_number)
+    # check if passed acn exists or not 
+    attrs = self::ATTRS 
+    refs = get_class_refs
 
-  def self.find_by_id
-    #code here
-  end
+    refs.each do |obj|
+      if obj.account_number == account_number 
+        return "Account balance for account number => #{obj.account_balance}"
+      end 
 
-  def self.all
-    result = ""
-    @@accnt_refs.each do |obj|
-       result += obj.view_account
-       result += ' , '
-    end 
-    result 
-    
-  end
-
-  private 
-  
-  # Delete account refrence 
-  def self.delete(obj)
-    if !exists?(obj)
-      return 'oops we didnt found information you request!!!'
-    end 
-    @@accnt_refs.delete(obj)
-  end
-
-  # Add new account 
-  def self.add(data_hash = {})
-    temp_obj = self.new
-    
-    self::ATTRS.each do |var|
-      value = data_hash.fetch(var)
-      temp_obj.instance_variable_set("@#{var}" , value) 
     end
 
-    @@accnt_refs << temp_obj
-    #returning object
-    temp_obj 
-
-  end
-
-  # check if object exist in global obj_refs array 
-  def self.exists?(obj)
-    (@@accnt_refs.include? obj)? true : false 
-  end
-  
-  # count number of objects 
-  def self.count
-    "object count:#{@@accnt_refs.size }"
+    return "xxxxx NO DATA FOUND xxxxx"
   end 
-  
-=end 
+
+  def self.credit(account_number, amount)
+    # Get obj first 
+    p self 
+    acn_obj_ref = self.find_by_id(account_number)
+    current_balance = acn_obj_ref.account_balance.to_i
+    new_balance = current_balance + amount.to_i
+    acn_obj_ref.account_balance= new_balance
+    # then do the maths 
+  end
+
+  def self.debit
+    # reduce blance
+  end
+
 end
