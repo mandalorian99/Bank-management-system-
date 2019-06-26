4d4819e6f349280a5f3dc83f889f1b937ea992f8
mahendra choudhary <mail.mahendrachoudhary@yahoo.com>
2019-06-26 13:49:45 +0530

    creating a console menu for app

--- app.rb
+++ app.rb
@@ -7,82 +7,107 @@ require './account.rb'
 class App
 
   def self.launch
+    display_menu_1
+  end
 
-    #creating a customer    
-    c1 = {'customer_id':101 , 'name':'john doe' , 'email':'doe@john.com' , 'phone':'893784'}
-    customer = Customer.create(c1)
+  def self.display_menu_1
+    options = ["Manage Customers" , "Manage  Employee" , "Manage Accounts" , "Exit"]
+    system('clear')
+    puts "\n\n\n\n"
+    puts "\t\t\t================================="
+    puts "\t\t\t   Welcome to Bank Of Mahendra "
+    puts "\t\t\t================================="
 
-    p '----reading customer data ----'
-    p customer.read    
-    
-    c2 = {'customer_id':102 , 'name':'abhram' , 'email':'god@lord.com' , 'phone':'999'}
-    customer2 = Customer.create(c2)
+    (options.size).times do |count|
+      puts "  \t\t\t [#{count+1}]  #{options[count]}" 
+    end 
+    print "\t\t\t Enter your choice : "
+    choice = gets.chomp
 
-    Customer.all
-    p customer.delete
-    p Customer.count
-
-    p "=============="
-    e1 = {'emp_id':1002 , 'name':'eva' ,'email':'eva@gmail.com','phone':'94783'}
-    emp1 = Employe.create(e1) 
-    p emp1.read
-    e2 = {'emp_id':1003 , 'name':'angel' ,'email':'angel@hotmail.com','phone':'0000'}
-    emp2 = Employe.create(e2)
-    
-    Employe.all
-    p Employe.count
-    emp1.delete 
-    p emp1.read 
-    p Employe.count 
-
-
-
-   a1 = {
-     'id':3001 ,
-     'account_number':'60033678' ,
-     'account_type':'saving' ,
-     'account_balance': 3500
-   }
-
-   accn = Account.create(a1)
-   p accn.read
-   #p accn.inspect
-   
-   # creating second account for test
-   a2 = {
-     'id':3002 ,
-     'account_number':'60000999' ,
-     'account_type':'current' ,
-     'account_balance': 201
-   }
-
-   accn2 = Account.create(a2)
-   #p Account.count
-
-   #Account.all
-
-   p "---------------List of all account --------"
-    puts Account.balance?('600009989')
-
-    p "---------depositing amount into acount-----------"
-     puts Account.balance?('60000999')
-     p Customer.deposite('60000999' , '3000')
-     p Customer.widraw('60000999','1000') 
-     puts Account.balance?('60000999') 
-    p '-------transfering amount--------'
-    p Customer.transfer('60000999' , '60033678' , '300')
-=begin
-   p '---------customer balance enquirey------'
-   puts Customer.new.balance?(60033678)
-    
-   Account.delete(accn2)
-   p Account.count
-   p Account.delete(accn2)
-   p Account.all
-
-=end
+    switcher(choice)
 
   end
+
+  def self.display_menu_2
+    options = ["Create Customers" , "Show Details" ,"Delete Customer" , "Update Customer" , "Exit"]
+    system('clear')
+    puts "\n\n\n\n"
+    puts "\t\t\t================================="
+    puts "\t\t\t   Manage Customers "
+    puts "\t\t\t================================="
+
+    (options.size).times do |count|
+      puts "  \t\t\t [#{count+1}]  #{options[count]}" 
+    end 
+    print "\t\t\t Enter your choice : "
+
+    case(gets.chomp) 
+    when '1'
+      puts "creating "
+    when '2'
+      puts "reading"
+    when '3'
+      puts "deleting"
+    when '4'
+      puts "updating"
+    when '5'
+      exit
+    else 
+      puts "invalid choice"
+    end 
+
+  end
+
+  def self.display_menu_3
+    options = ["Create Employee" , "Delete Employee" , "Update Employee" , "Exit"]
+    system('clear')
+    puts "\n\n\n\n"
+    puts "\t\t\t================================="
+    puts "\t\t\t   Manage Employee "
+    puts "\t\t\t================================="
+
+    (options.size).times do |count|
+      puts "  \t\t\t [#{count+1}]  #{options[count]}" 
+    end 
+    print "\t\t\t Enter your choice : "
+    choice = gets.chomp
+
+    switcher(choice)
+
+  end
+
+  def self.display_menu_4
+    options = ["Create Account" , "Delete Account" , "Update Account" , "Exit"]
+    system('clear')
+    puts "\n\n\n\n"
+    puts "\t\t\t================================="
+    puts "\t\t\t   Manage Accounts "
+    puts "\t\t\t================================="
+
+    (options.size).times do |count|
+      puts "  \t\t\t [#{count+1}]  #{options[count]}" 
+    end 
+    print "\t\t\t Enter your choice : "
+    choice = gets.chomp
+
+    switcher(choice)
+
+  end
+
+  def self.switcher(choice)
+    case(choice)
+    when '1'
+      display_menu_2
+    when '2'
+      display_menu_3
+    when '3'
+      display_menu_4
+    when '4'
+      display_menu_1
+    else 
+      puts "\t\t\t xxxxxx You need to consult a eye doctor xxxxxx"
+    end 
+  end 
 
 end 
 
